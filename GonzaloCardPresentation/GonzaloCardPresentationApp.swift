6f72db8b8aa5faa3b033a6ab0549225b29435b7f
Gonzalo Trujillo Vallejo <4bctrujillog1112@gmail.com>
2023-01-21 14:00:50 +0100

    Initial Commit

---
+++ GonzaloCardPresentation/GonzaloCardPresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GonzaloCardPresentationApp.swift
+//  GonzaloCardPresentation
+//
+//  Created by gonzalo trujillo vallejo on 21/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GonzaloCardPresentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
